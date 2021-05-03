2a6c7bc002e0a478442e90d78b39b2aeb71f3ad4
Saravana Kumar <saravanannkl@gmail.com>
2021-05-03 12:03:53 +0530

    Use columns from the json directly and provide overrides

--- data_converter.py
+++ data_converter.py
@@ -5,60 +5,60 @@ import csv
 DATA_MAPPING = {
     "vaccination_site_count": {
         "type": "object",
-        "columns": ["total", "govt", "pvt", "today"],
+        # "columns": ["total", "govt", "pvt", "today"],
         "path": "topBlock.sites"
     },
     "registration_count": {
         "type": "object",
-        "columns": ["total", "male", "female", "others", "online", "onspot", "today", "flwAndHcw"],
+        # "columns": ["total", "male", "female", "others", "online", "onspot", "today", "flwAndHcw"],
         "path": "topBlock.registration"
     },
     "vaccination_session_count": {
         "type": "object",
-        "columns": ["total", "govt", "pvt", "today"],
+        # "columns": ["total", "govt", "pvt", "today"],
         "path": "topBlock.sessions"
 
     },
     "vaccination_count": {
         "type": "object",
-        "columns": [
-            "total",
-            "male",
-            "female",
-            "others",
-            "covishield",
-            "covaxin",
-            "today",
-            "tot_dose_1",
-            "tot_dose_2",
-            "total_doses",
-            "aefi"
-        ],
+        # "columns": [
+        #     "total",
+        #     "male",
+        #     "female",
+        #     "others",
+        #     "covishield",
+        #     "covaxin",
+        #     "today",
+        #     "tot_dose_1",
+        #     "tot_dose_2",
+        #     "total_doses",
+        #     "aefi"
+        # ],
         "path": "topBlock.vaccination"
     },
     "vaccination_by_age": {
         "type": "object",
-        "columns": ["total", "vac_18_25", "vac_25_40", "vac_40_60", "above_60"],
+        # "columns": ["total", "vac_18_30", "vac_30_45", "vac_45_60", "above_60"],
         "path": "vaccinationByAge"
     },
     "session_vaccination_count": {
         "type": "object_list",
-        "columns": ["ts", "timestamps", "label", "count", "dose_one", "dose_two"],
+        # "columns": ["ts", "timestamps", "label", "count", "dose_one", "dose_two"],
         "path": "vaccinationDoneByTime"
     },
     "last_7days_registration_count": {
         "type": "object_list",
-        "columns": ["reg_date", "total", "male", "female", "others"],
+        # "columns": ["reg_date", "total", "male", "female", "others"],
         "path": "last7DaysRegistration"
     },
     "last_7days_vaccination_count": {
         "type": "object_list",
-        "columns": ["vaccine_date", "count", "dose_one", "dose_two", "covishield", "covaxin", "aefi"],
+        # "columns": ["vaccine_date", "count", "dose_one", "dose_two", "covishield", "covaxin", "aefi"],
         "path": "last7DaysVaccination"
     },
     "last_5days_session_status": {
         "type": "object_list",
-        "columns": ["session_date", "total", "planned", "ongoing", "completed"],
+        # "columns": ["session_date", "total", "planned", "ongoing", "completed"],
         "path": "last5daySessionStatus"
     },
     "meta": {
@@ -69,19 +69,20 @@ DATA_MAPPING = {
     "state_level_vaccination_count": {
         "location_type": "national",
         "type": "object_list",
-        "columns": ["state_id", "state_name", "total", "partial_vaccinated", "totally_vaccinated", "today"],
+        # "columns": ["state_id", "state_name", "total", "partial_vaccinated", "totally_vaccinated", "today"],
+        "exclude_columns": ["title"],
         "path": "getBeneficiariesGroupBy"
     },
     "district_level_vaccination_count": {
         "location_type": "state",
         "type": "object_list",
-        "columns": ["state_id", "district_id", "district_name", "total", "partial_vaccinated", "totally_vaccinated", "today"],
+        # "columns": ["state_id", "district_id", "district_name", "total", "partial_vaccinated", "totally_vaccinated", "today"],
         "path": "getBeneficiariesGroupBy"
     },
     "site_level_vaccination_count": {
         "location_type": "district",
         "type": "object_list",
-        "columns": ["session_site_id", "session_site_name", "total", "partial_vaccinated", "totally_vaccinated", "today"],
+        # "columns": ["session_site_id", "session_site_name", "total", "partial_vaccinated", "totally_vaccinated", "today"],
         "path": "getBeneficiariesGroupBy"
     }
 }
@@ -112,16 +113,24 @@ def get_nested_object(obj, path_key):
     return value
 
 
-def convert_object_to_csv(obj, date, columns, file_path):
+def convert_object_to_csv(obj, date, config, file_path):
     """
         Converts JSON object into a single row CSV file.
 
         Parameters:
             obj: JSON object for which the values needs to be extracted.
             date: Date value that needs to be added at start
-            columns: List of fields that needs to be extracted.
+            configuration: List of fields that needs to be extracted.
             file_path: Location where the CSV file needs to be saved.
     """
+    columns = config.get("columns")
+    exclude_columns = config.get("exclude_columns")
+    if not columns:
+        columns = list(obj.keys())
+    if exclude_columns:
+        for column in exclude_columns:
+            if column in columns:
+                columns.remove(column)
     with open(file_path, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow(["date", *columns])
@@ -129,7 +138,7 @@ def convert_object_to_csv(obj, date, columns, file_path):
         csv_writer.writerow([date, *data])
 
 
-def convert_object_array_to_csv(obj_list, date, columns, file_path):
+def convert_object_array_to_csv(obj_list, date, config, file_path):
     """
         Converts JSON array of objects into a multi row CSV file.
 
@@ -139,6 +148,15 @@ def convert_object_array_to_csv(obj_list, date, columns, file_path):
             columns: List of fields that needs to be extracted.
             file_path: Location where the CSV file needs to be saved.
     """
+    columns = config.get("config")
+    exclude_columns = config.get("exclude_columns")
+    if not columns:
+        obj = obj_list[0] if len(obj_list) > 0 else {}
+        columns = list(obj.keys())
+    if exclude_columns:
+        for column in exclude_columns:
+            if column in columns:
+                columns.remove(column)
     with open(file_path, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow(["date", *columns])
@@ -168,7 +186,7 @@ def convert_data(location_type, folder, data):
         file_path = os.path.join(folder, "{}.csv".format(data_type))
         if mapping_type == "object":
             convert_object_to_csv(
-                obj, data["date"], config["columns"], file_path)
+                obj, data["date"], config, file_path)
         elif mapping_type == "object_list":
             convert_object_array_to_csv(
-                obj, data["date"], config["columns"], file_path)
+                obj, data["date"], config, file_path)
